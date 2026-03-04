0b7b8c0c572da22a4421bf1b045b51c96e7453aa
komed3 <pk97@outlook.de>
2026-03-04 18:23:59 +0100

    add some more nodes

--- src/ast.ts
+++ src/ast.ts
@@ -38,4 +38,45 @@ export class ConstantNode extends ASTNode {
     constructor ( public name: string, public value: number, position?: Position ) {
         super( position );
     }
+
+}
+
+export class BinaryOpNode extends ASTNode {
+
+    protected type = 'BinaryOp';
+
+    constructor ( public operator: string, public left: ASTNode, public right: ASTNode, position?: Position ) {
+        super( position );
+    }
+
+}
+
+export class UnaryOpNode extends ASTNode {
+
+    protected type = 'UnaryOp';
+
+    constructor ( public operator: string, public operand: ASTNode, position?: Position ) {
+        super( position );
+    }
+
+}
+
+export class FunctionNode extends ASTNode {
+
+    protected type = 'Function';
+
+    constructor ( public name: string, public args: ASTNode[], position?: Position ) {
+        super( position );
+    }
+
+}
+
+export class GroupNode extends ASTNode {
+
+    protected type = 'Group';
+
+    constructor ( public expression: ASTNode, position?: Position ) {
+        super( position );
+    }
+
 }
