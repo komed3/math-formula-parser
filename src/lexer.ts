cec8f8724cdcc0baa6eff85d6e829c6d7792761f
komed3 <pk97@outlook.de>
2026-03-04 19:13:20 +0100

    tokenize input string

--- src/lexer.ts
+++ src/lexer.ts
@@ -10,4 +10,25 @@ export class Lexer {
 
     constructor ( private input: string ) {}
 
+    public tokenize () : Token[] {
+        this.tokens = [];
+        this.position = 0;
+        this.line = 1;
+        this.column = 1;
+
+        while ( this.position < this.input.length ) {
+            if ( !this.scanToken() ) {
+                throw new Error( `Lexer error at ${this.line}:${this.column}` );
+            }
+        }
+
+        this.tokens.push( {
+            type: TokenType.EOF,
+            value: '',
+            position: this.getPos(),
+        } );
+
+        return this.tokens;
+    }
+
 }
