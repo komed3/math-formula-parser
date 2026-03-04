9b9d176fd8cf744641b2df48be1418271a97ae50
komed3 <pk97@outlook.de>
2026-03-04 18:22:16 +0100

    implement number / var / const nodes

--- src/ast.ts
+++ src/ast.ts
@@ -10,3 +10,32 @@ export abstract class ASTNode {
     }
 
 }
+
+export class NumberNode extends ASTNode {
+
+    protected type = 'Number';
+
+    constructor ( public value: number, position?: Position ) {
+        super( position );
+    }
+
+}
+
+export class VariableNode extends ASTNode {
+
+    protected type = 'Variable';
+
+    constructor ( public name: string, position?: Position ) {
+        super( position );
+    }
+
+}
+
+export class ConstantNode extends ASTNode {
+
+    protected type = 'Constant';
+
+    constructor ( public name: string, public value: number, position?: Position ) {
+        super( position );
+    }
+}
