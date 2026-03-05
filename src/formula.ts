1532ff7a76e361903d7a3bde58d6f3289c148ac7
komed3 <pk97@outlook.de>
2026-03-05 11:08:32 +0100

    get instruction set

--- src/formula.ts
+++ src/formula.ts
@@ -1,4 +1,5 @@
 import type { ASTNode } from './types';
+import { MATH_CONSTANTS, MATH_FUNCTIONS } from './constants';
 import { Lexer } from './lexer';
 import { Parser } from './parser';
 import { Visualizer } from './visualizer';
@@ -6,6 +7,16 @@ import { Visualizer } from './visualizer';
 export class MathFormulaParser {
 
     private static readonly visualizer = new Visualizer();
+
+    public static instructionSet () {
+        return {
+            version: '0.1.0-alpha',
+            constants: Object.keys( MATH_CONSTANTS ),
+            functions: Array.from( MATH_FUNCTIONS ).sort(),
+            operators: [ '+', '-', '*', '/', '^', '%', '==', '!=', '<', '>', '<=', '>=', '&&', '||', '!', '...' ],
+            specialOps: [ 'integral', 'sum', 'product', 'sqrt', 'd/dx', 'partial' ]
+        };
+    }
 
     public parse ( formula: string ) : ASTNode {
         const parser = new Parser( ( new Lexer( formula ) ).tokenize() );
