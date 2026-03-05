f4ef30e65ecf78aeba660b905e81d04d9faabbaa
komed3 <pk97@outlook.de>
2026-03-05 11:06:38 +0100

    parse formula

--- src/formula.ts
+++ src/formula.ts
@@ -1,3 +1,4 @@
+import type { ASTNode } from './types';
 import { Lexer } from './lexer';
 import { Parser } from './parser';
 import { Visualizer } from './visualizer';
@@ -5,5 +6,10 @@ import { Visualizer } from './visualizer';
 export class MathFormulaParser {
 
     private static readonly visualizer = new Visualizer();
+
+    public parse ( formula: string ) : ASTNode {
+        const parser = new Parser( ( new Lexer( formula ) ).tokenize() );
+        return parser.parse();
+    }
 
 }
