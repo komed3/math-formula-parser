86d2f6b3c8e3cd957aca950fdd59e7caad23a004
komed3 <pk97@outlook.de>
2026-03-05 09:32:24 +0100

    add public parse() method

--- src/parser.ts
+++ src/parser.ts
@@ -1,9 +1,16 @@
-import type { Token } from './types';
+import type { ASTNode, Token } from './types';
 
 export class Parser {
 
     private current = 0;
 
     constructor ( private tokens: Token[] ) {}
+
+    public parse () : ASTNode {
+        const expr = this.assignment();
+
+        if ( ! this.isAtEnd() ) throw this.error( 'Unexpected token after expression' );
+        return expr;
+    }
 
 }
