cfb5e4b9b35c77347299a6a48d6fca841de24061
komed3 <pk97@outlook.de>
2026-03-06 09:38:45 +0100

    Update examples.ts

--- test/examples.ts
+++ test/examples.ts
@@ -1,3 +1,8 @@
 import { MathFormulaParser } from '../src';
 
 const parser = new MathFormulaParser();
+
+// Basic parsing
+const ast1 = parser.parse( '2 + 3 * 4' );
+console.log( 'Input: 2 + 3 * 4' );
+console.log( 'String:', parser.toString( ast1 ) );
