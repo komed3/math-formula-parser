6f7e8b63cb2fdd5b73495e4b60912c329a50d17a
komed3 <pk97@outlook.de>
2026-03-07 09:20:00 +0100

    fully rework of the AST node class

--- src/ast.ts
+++ src/ast.ts
@@ -1,200 +1,33 @@
-import { Position } from './types';
+import type { Position, VisualizationOptions } from './types';
+import { NodeFormatter } from './definitions';
 
-abstract class ASTNode {
-    protected type!: string;
-    protected position?: Position;
+export class ASTNode {
 
-    constructor ( position?: Position ) {
+    public readonly kind: string;
+    public readonly props: Record< string, any >;
+    public readonly position?: Position;
+
+    constructor ( kind: string, props: Record< string, any > = {}, position?: Position ) {
+        this.kind = kind;
+        this.props = props;
         this.position = position;
     }
-}
 
-export class NumberNode extends ASTNode {
-    protected type = 'Number';
-    constructor ( public value: number, position?: Position ) {
-        super( position );
+    public children () : ASTNode[] {
+        return NodeFormatter.getChildren( this );
     }
-}
 
-export class VariableNode extends ASTNode {
-    protected type = 'Variable';
-
-    constructor ( public name: string, position?: Position ) {
-        super( position );
+    public label ( options: VisualizationOptions = {} ) : string {
+        return NodeFormatter.getLabel( this, options );
     }
-}
 
-export class ConstantNode extends ASTNode {
-    protected type = 'Constant';
-
-    constructor ( public name: string, public value: number, position?: Position ) {
-        super( position );
+    public toString ( parentPrecedence = 0 ) : string {
+        return NodeFormatter.toString( this, parentPrecedence );
     }
-}
 
-export class BinaryOpNode extends ASTNode {
-    protected type = 'BinaryOp';
-
-    constructor (
-        public operator: string, public left: ASTNode, public right: ASTNode,
-        position?: Position
-    ) {
-        super( position );
+    public walk ( fn: ( node: ASTNode, depth: number ) => void, depth = 1 ) : void {
+        fn( this, depth );
+        for ( const child of this.children() ) child.walk( fn, depth + 1 );
     }
-}
 
-export class UnaryOpNode extends ASTNode {
-    protected type = 'UnaryOp';
-
-    constructor ( public operator: string, public operand: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class FunctionNode extends ASTNode {
-    protected type = 'Function';
-
-    constructor ( public name: string, public args: ASTNode[], position?: Position ) {
-        super( position );
-    }
-}
-
-export class GroupNode extends ASTNode {
-    protected type = 'Group';
-
-    constructor ( public expression: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class SqrtNode extends ASTNode {
-    protected type = 'Sqrt';
-
-    constructor ( public radicand: ASTNode, public degree?: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class PowerNode extends ASTNode {
-    protected type = 'Power';
-
-    constructor ( public base: ASTNode, public exponent: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class SummationNode extends ASTNode {
-    protected type = 'Summation';
-
-    constructor (
-        public variable: string, public lower: ASTNode, public upper: ASTNode,
-        public expression: ASTNode, position?: Position
-    ) {
-        super( position );
-    }
-}
-
-export class ProductNode extends ASTNode {
-    protected type = 'Product';
-
-    constructor (
-        public variable: string, public lower: ASTNode, public upper: ASTNode,
-        public expression: ASTNode, position?: Position
-    ) {
-        super( position );
-    }
-}
-
-export class IntegralNode extends ASTNode {
-    protected type = 'Integral';
-
-    constructor (
-        public variable: string, public expression: ASTNode, public lower?: ASTNode,
-        public upper?: ASTNode, position?: Position
-    ) {
-        super( position );
-    }
-}
-
-export class DerivativeNode extends ASTNode {
-    protected type = 'Derivative';
-
-    constructor ( public variable: string, public expression: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class PartialDerivativeNode extends ASTNode {
-    protected type = 'PartialDerivative';
-
-    constructor ( public variable: string, public expression: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class VectorNode extends ASTNode {
-    protected type = 'Vector';
-
-    constructor ( public elements: ASTNode[], position?: Position ) {
-        super( position );
-    }
-}
-
-export class MatrixNode extends ASTNode {
-    protected type = 'Matrix';
-
-    constructor ( public rows: ASTNode[][], position?: Position ) {
-        super( position );
-    }
-}
-
-export class ComplexNode extends ASTNode {
-    protected type = 'Complex';
-
-    constructor ( public real: ASTNode, public imaginary: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class IndexNode extends ASTNode {
-    protected type = 'Index';
-
-    constructor ( public base: ASTNode, public index: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class SubscriptNode extends ASTNode {
-    protected type = 'Subscript';
-
-    constructor ( public base: ASTNode, public subscript: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class RangeNode extends ASTNode {
-    protected type = 'Range';
-
-    constructor (
-        public lower: ASTNode, public upper: ASTNode, public lowerInclusive: boolean,
-        public upperInclusive: boolean, position?: Position
-    ) {
-        super( position );
-    }
-}
-
-export class EllipsisNode extends ASTNode {
-    protected type = 'Ellipsis';
-
-    constructor ( public left: ASTNode, public right: ASTNode, position?: Position ) {
-        super( position );
-    }
-}
-
-export class FactorialNode extends ASTNode {
-    protected type = 'Factorial';
-
-    constructor ( public operand: ASTNode, position?: Position ) {
-        super( position );
-    }
 }
