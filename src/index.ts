29ad98411158c69fbe282abf76b71388c3ff6f55
komed3 <pk97@outlook.de>
2026-03-24 10:53:51 +0100

    add package intro

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+/**
+ * Math Formula Parser is a flexible library for parsing and analyzing mathematical formulas.
+ * 
+ * It provides tools to tokenize, parse into an Abstract Syntax Tree (AST), and perform various
+ * analyses on formulas, such as extracting variables, constants, and functions, as well as
+ * calculating formula depth and node counts.
+ * 
+ * The library is designed to be lightweight, easy to use, and extensible, making it suitable
+ * for a wide range of applications that involve processing mathematical expressions.
+ * 
+ * @author Paul Köhler
+ * @license MIT
+ * @version 0.1.0 [BETA]
+ */
+
 export { CONSTANTS, FUNCTIONS, OPERATORS } from './definitions';
 export { Formula } from './formula';
 export { Visualizer } from './visualizer';
